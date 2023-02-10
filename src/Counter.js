91e6901fa60e15b39d65a86ac0ad86f34f8756e1
simple-sifu <kattyntommy@gmail.com>
2023-02-10 15:30:57 -0500

    added css styling to sample index1.html

---
+++ src/Counter.js
@@ -0,0 +1,26 @@
+// React - allows us to write JSX
+// Component - create a class
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            count: props.initialCount
+        }
+    }
+    render(){
+        return(
+            <div>
+            <button onClick={()=> this.changeCount(-1)}>-</button>
+            <span>{this.state.count}</span>
+            <button onClick={() => this.changeCount(1)}>+</button>
+          </div>
+        )
+    }
+
+    changeCount(amount){
+        this.setState(prevState => { return {count: prevState.count + amount } })
+    }
+}
